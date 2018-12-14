31b371abb4f179ba32454882a8f9c28ec298c9d7
ouyangdan <ouyangdan@ewang.com>
2018-12-14 15:45:52 +0800

    hello world-doYu

--- doYu/AppDelegate.swift
+++ doYu/AppDelegate.swift
@@ -15,7 +15,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+       
+        //设置全局UITabBar颜色
+        UITabBar.appearance().tintColor=UIColor.gray;
+        
         return true
     }
 
---
+++ doYu/Ca/Me/MeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MeViewController.swift
+//  doYu
+//
+//  Created by henry on 2018/12/14.
+//  Copyright © 2018 henry. All rights reserved.
+//
+
+import UIKit
+
+class MeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
